b87b61f1306b93a45f5039d4986d15a5431472e4
hoang <particle4dev@gmail.com>
2017-02-10 20:23:34 +0700

    removed TaskScheduler Model

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,2 @@
 export { default as TasksModel } from './tasks-schema';
 export { default as NodesModel } from './nodes-schema';
-export { default as TasksSchedulerModel } from './tasks-scheduler-schema';
